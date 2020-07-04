878feaf43d70184f366b5d4292dd6507390aee31
Alexander Netto Scott <ascott11@umd.edu>
2020-07-04 14:08:56 -0400

    fuck

--- stocks.py
+++ stocks.py
@@ -10,13 +10,22 @@ import psycopg2
 import datetime
 import matplotlib.pyplot as plt
 import random
+import signal
+import sys
 from pandas_datareader import data as pdr
 from bs4 import BeautifulSoup
 
 class Stock():
     
-    def __init__(self, name):
+    def __init__(self, name, P1, P2, P3, P4):
         self.name = name
+        self.P1 = P1
+        self.P2 = P2
+        self.P3 = P3
+        self.P4 = P4
+
+    def to_string(self):
+        return "P1: {}, P2: {}, P3: {}, P4: {}".format(self.P1, self.P2, self.P3, self.P4)
 
 def get_Dow():
     Dow_Website = requests.get("https://money.cnn.com/data/dow30/")
@@ -104,21 +113,60 @@ if __name__=="__main__":
             continue
 
     all_stocks = list()
-    d = dict()
     legends = list()
-    for i in Tickers:
+    stocks_class = list()
+    for day in range((end - (start)).days):
+        
+        mu = 0
+        for tag in Tickers:
+            try:
+                opn = stock_data[tag].loc[day, "Open"]
+                close = stock_data[tag].loc[day, "Close"]
+                mu = mu + ((close-opn)/opn)
+            except:
+                continue
+
+        mu = mu / len(Tickers)
+
+        for tag in Tickers:
+            curr_day = start + datetime.timedelta(days=day)
+            
+            try:
+                opn = stock_data[tag].loc[day, "Open"]
+                close = stock_data[tag].loc[day, "Close"]
+                low = stock_data[tag].loc[day, "Low"]
+                high = stock_data[tag].loc[day, "High"]
+                adj = stock_data[tag].loc[day, "Adj Close"]
+
+                stocks_class.append(Stock(tag, (close - opn)/opn, args.i/len(Tickers), \
+                    ((close - opn)/opn) - mu, adj - close))
+            except:
+                continue
+        
+    """for i in stock_data.keys():
+        for index, row in stock_data[i].iterrows():
+            print("INDEX: {}, ROW: {}, NAME: {}".format(index, row, i))"""
+
+    for stock in stocks_class:
+        print(stock.to_string())
+
+
+    """for i in Tickers:
         for j in range((end - (start)).days):
             day = start + datetime.timedelta(days=j)
             try:
                 d[day] = stock_data[i].loc[day, "High"]
+                opn = stock_data[i].loc[day, "Open"]
+                close = stock_data[i].loc[day, "Close"]
+                low = stock_data[i].loc[day, "Low"]
+                high = stock_data[i].loc[day, "High"]
+
+                stocks_class.append(Stock(tag, (opn - close)/ open, args.i/len(Tickers)))
             except:
                 continue
+
         color = (random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1))
-        plot = plt.plot_date(d.keys(), d.values(), 'b-', label=i, c=color)
+        plot = plt.plot_date(d.keys(), d.values(), 'b-', label=i, c=color)"""
 
-    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
-    plt.show()
-
-"""for i in stock_data.keys():
-        for index, row in stock_data[i].iterrows():
-            print("INDEX: {}, ROW: {}, NAME: {}".format(index, row, i))"""
+    #plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+    #plt.show()
